21d438a382c637774e7736e9e6ef77205a12e8b6
PedroEduardoSS <pedroeduardossousa@gmail.com>
2021-05-10 10:57:45 -0300

    Update project's structure

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,11 +1,14 @@
 from dearpygui.core import *
-from dearpygui.simple import * 
+from dearpygui.simple import *
+import pandas as pd
 
 def dataframes(sender, data):
     with window("DataFrame"):
         with tab_bar("tabbar"):
             with tab("Management ##dataframes"):
                 add_text("For now, It is okay", bullet=True)
+                add_separator()
+                add_table("Default", ["col1", "col2", "col3"])
             with tab("Settings ##dataframes"):
                 add_text("Test", bullet=True)
             with tab("Help ##dataframes"):
