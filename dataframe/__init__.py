aa01b3a4198ed135166e11e9a5b551f165199bce
PedroEduardoSS <pedroeduardossousa@gmail.com>
2021-06-04 17:22:08 -0300

    Main dataframe functions finished

--- dataframe/__init__.py
+++ dataframe/__init__.py
@@ -1,29 +1,50 @@
 from dearpygui.core import *
 from dearpygui.simple import *
+import pandas as pd
 
 def add_rows():
     row_datas = get_value("Row data")
     row_data_list = row_datas.split(",")
-    add_row("Table", row_data_list)
+    add_row("Table ##creation", row_data_list)
 
 def add_headers(sender, data):
     column_name = get_value("Column name")
     columns_list = column_name.split(",")
-    set_headers("Table", columns_list)
+    set_headers("Table ##creation", columns_list)
+
+def add_csv_file(sender, data):
+    dataframe = pd.read_csv(get_value("CSV file path"))
+    columns = dataframe.columns
+    columns_list = []
+    for column in columns:
+        columns_list.append(column)
+    set_headers("Table ##manipulation", columns_list)
+
+    rows = dataframe.index
+    for row in rows:
+        id_row = dataframe.loc[row].to_list()
+        add_row("Table ##manipulation", id_row)
+    configure_item("Table ##manipulation", show=True)
 
 def dataframes(sender, data):
     with window("DataFrame", width=400, height=300, no_close=True):
         with tab_bar("tabbar ##dataframes"):
-            with tab("Management ##dataframes"):
-                add_text("Create/manipulate dataframes here", bullet=True)
+            with tab("Creation ##dataframes"):
+                add_text("Create dataframes here", bullet=True)
                 add_separator()
                 add_input_text("Column name", hint="col name1,col name2", tip="Each column name\nmust be separated by comma")
                 add_button("Add columns", callback=add_headers)
                 add_input_text("Row data", hint="data col1,data col2", tip="Each row data\nmust be separated by comma")
                 add_button("Add row", callback=add_rows)
-                add_button("tabledata", callback=lambda:print(get_table_data("Table")))
-                add_table("Table", ["col1", "col2", "col3"])
+                add_table("Table ##creation", ["col1", "col2", "col3"])
+
+            with tab("Manipulation ##dataframes"):
+                add_text("Manipulate dataframes here", bullet=True)
                 add_separator()
+                add_input_text("CSV file path")
+                add_button("Add CSV file", callback=add_csv_file)
+                add_table("Table ##manipulation", ["Wait", "Wait"], show=False)
+
 
             with tab("Settings ##dataframes"):
                 add_text("Test", bullet=True)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+from dearpygui.simple import * 
+from dearpygui.core import *
