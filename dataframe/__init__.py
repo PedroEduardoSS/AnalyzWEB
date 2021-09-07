78838a01d690c48d44328a2764724d777344f9ec
PedroEduardoSS <pedroeduardossousa@gmail.com>
2021-09-07 14:04:30 -0300

    finished dataframes

--- dataframe/__init__.py
+++ dataframe/__init__.py
@@ -37,11 +37,12 @@ def create_table(sender, data):
 
 def delete_table(sender, data):
     set_headers("Table ##handling", ["Header", "Header", "Header"])
-    set_table_data("Table ##handling", data=[])
+    clear_table("Table ##handling")
 
 #csv file-handling
 def add_csv_file(sender, data):
-    dataframe = pd.read_csv(get_value("CSV file path"))
+    file_name = get_value("CSV file path")
+    dataframe = pd.read_csv(f"CSV-Files\{file_name}")
     columns = dataframe.columns
     columns_list = []
     for column in columns:
@@ -56,9 +57,10 @@ def add_csv_file(sender, data):
 
 #ATENTION
 def save_as_csv_data(sender, data):
-    rows = get_table_selections("Table ##handling")
+    columns = get_value("Columns names").split(",")
+    rows = get_table_data("Table ##handling")
     file_name = get_value("CSV file name")
-    data = pd.DataFrame(rows)
+    data = pd.DataFrame(rows, columns=columns)
     data.to_csv(f"CSV-Files\{file_name}.csv", index=False)
 
 #ATENTION end
@@ -67,58 +69,79 @@ def dataframes(sender, data):
     with window("DataFrame", width=400, height=300, no_close=True):
         with tab_bar("tabbar ##dataframes"):
             with tab("Handling ##dataframes"):
-                add_text("Manipulate dataframes here", bullet=True)
+                add_text("Manipulate the table here", bullet=True)
                 add_separator()
-                
-                add_text("Pay attention to the hint below\nMore information on 'Help' tab", bullet=True)
-                add_input_text("CSV file path", hint="CSV-Files\File.csv")
-                add_button("Add CSV file", callback=add_csv_file)
-                add_spacing(count=5)
 
-                add_text("Add the columns names below", bullet=True)
+                add_text("Initial commands", color=[255, 0, 0])
+                add_button("Create Table", callback=create_table)
+                add_same_line()
+                add_button("Delete Table", callback=delete_table)
+                add_spacing(count=3)
+                add_input_text("CSV file path", hint="myfile.csv")
+                add_button("Add CSV file", callback=add_csv_file)
+                
+                add_spacing(count=2)
+                add_separator()
+                add_spacing(count=2)
+
+                add_text("Columns", color=[255, 0, 0])
+                add_text("Add the columns names", bullet=True)
                 add_input_text("Column name ##handling", hint="col name1,col name2", tip="Each column name\nmust be separated by comma")
                 add_input_int("Column index ##handling")
                 add_input_text("Column values")
                 add_button("Add column ##handling", callback=add_headers_data)
-                add_spacing(count=5)
 
-                add_text("Update the columns names below", bullet=True)
+                add_spacing(count=3)
+
+                add_text("Update the columns names", bullet=True)
                 add_input_text("Columns ##handling", hint="col name1,col name2", tip="Each column name\nmust be separated by comma")
                 add_button("Update columns ##handling", callback=update_headers_data)
-                add_spacing(count=5)
+                
+                add_spacing(count=3)
 
                 add_text("Delete the column", bullet=True)
-                add_input_int("Column index")
+                add_input_int("Column index", tip="The index starts at 0")
                 add_button("Delete column ##handling", callback=remove_column)
-                add_spacing(count=5)
                 
+                add_spacing(count=2)
+                add_separator()
+                add_spacing(count=2)
+
+                add_text("Rows and Cells", color=[255, 0, 0])
                 add_text("Add data for each cell of a row", bullet=True)
                 add_input_text("Row data ##handling", hint="data col1,data col2", tip="Each row data\nmust be separated by comma")
                 add_button("Add row ##handling", callback=add_row_datas)
-                add_spacing(count=5)
+                
+                add_spacing(count=3)
 
                 add_text("Remove row", bullet=True)
                 add_input_int("Row index ##handling")
                 add_button("Remove row ##handling", callback=remove_row)
-                add_spacing(count=5)
+                
+                add_spacing(count=3)
 
                 add_text("Update the data from a specific cell", bullet=True)
                 add_input_text("Cell data ##handling", hint="data col1,data col2", tip="Each row data\nmust be separated by comma")
                 add_button("Update data ##handling", callback=update_cell_data)
-                add_spacing(count=5)
                 
+                add_spacing(count=2)
+                add_separator()
+                add_spacing(count=2)
+
+                add_text("Save", color=[255, 0, 0])
                 add_text("ATTENTION! You can only save the tables data", bullet=True)
+                add_input_text("Columns names")
                 add_input_text("CSV file name")
                 add_button("Save as CSV ##handling", callback=save_as_csv_data)
                 
-            with tab("Table"):
-                add_button("Create Table", callback=create_table)
-                add_same_line()
-                add_button("Delete Table", callback=delete_table)
+            with tab("Table ##dataframes"):
+                add_text("Table", bullet=True)
                 add_table("Table ##handling", ["Col1", "Col2", "Col3"], show=False)
             
             with tab("Help ##dataframes"):
-                add_text("It is your place to create or manipulate dataframes", bullet=True)
-                add_text("To open a CSV file on Manipulation tab,\n you must firstly add the csv file on 'CSV-Files' folder", bullet=True)
+                with collapsing_header("Handling Tab"):
+                    add_text("Test")
+                with collapsing_header("Table Tab"):
+                    add_text("Test2")
                 add_separator()
                 add_button("Close ##DataFrame", small=True, callback=lambda:delete_item("DataFrame"), tip="You must close the window here", label="Close Window")
