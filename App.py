a9360d48e41da55aac3514a0760b5cb22db09e31
PedroEduardoSS <pedroeduardossousa@gmail.com>
2025-03-28 19:24:04 -0300

    Add charts
    Four types of chart are being added (area, bar, line, scatter)

--- App.py
+++ App.py
@@ -4,7 +4,10 @@ from views.data_analysis_tab import *
 
 st.title("Data Analysis workplace")
 
-tab1 = st.tabs(["Data Analysis"])
+tab1, tab2 = st.tabs(["Data Analysis", "extraTab"])
 
 with tab1:
     data_analysis_tab()
+
+with tab2:
+    pass
