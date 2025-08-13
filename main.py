0f41ce726a77126d8ebf51eec6a9513f05e5f8d5
PedroEduardoSS <pedroeduardossousa@gmail.com>
2025-08-13 17:12:10 -0300

    Add markdown and dark mode

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from nicegui import ui
 import pandas as pd
 from views.data_analysis_tab import *
 
-ui.html("Data Analysis workplace", tag='h1')
+dark = ui.dark_mode()
+
+ui.markdown('''
+            # Data Analysis workplace
+            ''')
+
+ui.switch('Dark mode').bind_value(dark)
+
 
 with ui.tabs().classes('w-full') as tabs:
     tab1 = ui.tab('Data Analysis')
